1846ea9db37be89ac4ec7d72c681ac623fa5a96c
Helen Manders-Jones <realhelenmjones@gmail.com>
2023-07-25 10:55:27 +0100

    updated demo and docs

--- dist/nbnatlas-sdk-src.js
+++ dist/nbnatlas-sdk-src.js
@@ -42,7 +42,7 @@
      */
     const CONFIG = {
         URL_RECORDS_WS: 'https://records-ws.nbnatlas.org',
-        URL_SPECIES_WS: 'https://species-ws-dev.nbnatlas.org',
+        URL_SPECIES_WS: 'https://species-ws.nbnatlas.org',
         URL_LISTS_WS: 'https://lists.nbnatlas.org'
     };
 
