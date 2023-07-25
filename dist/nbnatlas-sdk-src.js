347f5eb2efc5308efbe6b78996dc389159b74ee2
Helen Manders-Jones <realhelenmjones@gmail.com>
2023-07-25 10:27:31 +0100

    build

--- dist/nbnatlas-sdk-src.js
+++ dist/nbnatlas-sdk-src.js
@@ -211,7 +211,14 @@
             const url = `${CONFIG.URL_RECORDS_WS}/occurrences/search?q=${layerId}:(${encodeAndJoin(placeNames)})&facets=names_and_lsid&pageSize=1&flimit=-1
                                 &fq=-occurrence_status:absent&sort=year&dir=desc&fl=year`;
             const json = await this._getJson(url);
-            return this._buildOccurrenceCountDTO(json)
+            const occurrenceCountDTO = this._buildOccurrenceCountDTO(json);
+            if (occurrenceCountDTO.length) {
+                await this._addLastOccurrenceYear({layerId, placeNames, speciesListId: undefined, occurrenceCountDTO});
+                const batCount = await this._getBatOccurrenceCount({layerId, placeNames});
+                occurrenceCountDTO.push(batCount);
+            }
+
+            return occurrenceCountDTO;
         }
 
         async getOccurrenceCountForSpeciesList({layerId, placeNames, speciesListId}) {
@@ -219,7 +226,37 @@
                             &fq=-occurrence_status:absent&fq=species_list_uid:${speciesListId}&sort=year&dir=desc&fl=year`;
 
             const json = await this._getJson(url);
-            return this._buildOccurrenceCountDTO(json)
+            const occurrenceCountDTO = this._buildOccurrenceCountDTO(json);
+            if (occurrenceCountDTO.length) {
+                await this._addLastOccurrenceYear({layerId, placeNames, speciesListId, occurrenceCountDTO});
+                const batCount = await this._getBatOccurrenceCount({layerId, placeNames});
+                occurrenceCountDTO.push(batCount);
+            }
+
+            return occurrenceCountDTO;
+        }
+
+        async _getBatOccurrenceCount({layerId, placeNames}) {
+            const url =`${CONFIG.URL_RECORDS_WS}/occurrences/search?q=${layerId}:(${encodeAndJoin(placeNames)})&fq=lsid:NHMSYS0000376160
+                            &fq=occurrence_status:present&pageSize=1&sort=year&dir=desc`;
+            const json = await this._getJson(url);
+
+            const occurrenceCountDTO = json.occurrences.length ?  {
+                count:json.totalRecords,
+                year:json.occurrences[0].year,
+                additional: {
+                    scientificName: "Chiroptera",
+                    commonName: "Bat",
+                    taxonGuid: "NHMSYS0020001355"
+                }} : {
+                count:0,
+                year:0,
+                additional: {
+                    scientificName: "Chiroptera",
+                    commonName: "Bat",
+                    taxonGuid: "NHMSYS0020001355"
+                }};
+            return occurrenceCountDTO;
         }
 
         /**
@@ -234,13 +271,13 @@
          */
         _buildOccurrenceCountDTO(json) {
             //let result = json.facetResults?.[0]?.fieldResult?.map(it => {
-            let year = json.occurrences && json.occurrences[0] && json.occurrences[0].year ? json.occurrences[0].year:0;
-            let result = json.facetResults && json.facetResults[0] && json.facetResults[0].fieldResult ?
+            // let year = json.occurrences && json.occurrences[0] && json.occurrences[0].year ? json.occurrences[0].year:0;
+            const result = json.facetResults && json.facetResults[0] && json.facetResults[0].fieldResult ?
                 json.facetResults[0].fieldResult.map(it => {
                     const label = it.label.split('|');
                     return {
                         ...it,
-                        year,
+                        year:0, //this will be calculated later
                         additional: {
                             scientificName: label[0],
                             commonName: label[2],
@@ -250,6 +287,39 @@
                 }) : [];
 
             return result;
+        }
+
+
+        /**
+         * @private
+         */
+        async _addLastOccurrenceYear({layerId, placeNames, speciesListId, occurrenceCountDTO}) {
+           // const totalCount = occurrenceCountDTO.reduce((acc, cur) => acc + cur.count, 0);
+                const taxnConceptGuids = "("+occurrenceCountDTO.map(item => item.additional.taxonGuid).join(' OR ')+")";
+
+                const url = `${CONFIG.URL_RECORDS_WS}/occurrences/search?q=${layerId}:(${encodeAndJoin(placeNames)})&pageSize=20000
+                                &fq=-occurrence_status:absent&fq=taxon_concept_lsid:${taxnConceptGuids}&sort=year&dir=desc&fl=year,taxon_concept_lsid`
+                + (speciesListId ? `&fq=species_list_uid:${speciesListId}` : '');
+
+                const json = await this._getJson(url);
+
+
+                const taxonConcepts = Object.values(json.occurrences.reduce((acc, cur) => {
+                    const { taxonConceptID, year } = cur;
+                    if (!acc[taxonConceptID] || year > acc[taxonConceptID].year) {
+                        acc[taxonConceptID] = { taxonConceptID, year };
+                    }
+                    return acc;
+                }, {}));
+
+
+            occurrenceCountDTO.forEach(item => {
+                const matchingConcept = taxonConcepts.find(concept => concept.taxonConceptID === item.additional.taxonGuid);
+                if (matchingConcept) {
+                    item.year = matchingConcept.year;
+                }
+            });
+            return occurrenceCountDTO;
         }
 
         /** @private */
@@ -377,7 +447,7 @@
 
             let result = [];
             const occurrenceCountDTO = await this.recordsWS.getOccurrenceCountForSpeciesList({layerId:this.layerId, placeNames, speciesListId});
-            if (occurrenceCountDTO) {
+            if (occurrenceCountDTO && occurrenceCountDTO.length) {
                 const sensitiveInWalesDTO = await this.listsWS.getSpeciesList(SPECIES_LIST.SENSITIVE_IN_WALES);
                 const sensitiveInEnglandDTO = await this.listsWS.getSpeciesList(SPECIES_LIST.SENSITIVE_IN_ENGLAND);
 
@@ -451,12 +521,12 @@
          * @private
          */
         _buildOccurrenceCountForSpeciesListResult(speciesListDTO, occurrenceCountDTO, sensitiveInEnglandDTO, sensitiveInWalesDTO) {
-            if (!occurrenceCountDTO) {
+            if (!occurrenceCountDTO || occurrenceCountDTO.length === 0) {
                 return {}
             }
             const england = this._sensitiveSpeciesJSONToMap(sensitiveInEnglandDTO);
             const wales = this._sensitiveSpeciesJSONToMap(sensitiveInWalesDTO);
-            return speciesListDTO.map(it => {
+            const result =  speciesListDTO.map(it => {
                 let countAndLastRecorded = this._getOccurrenceCountAndLastRecorded(it.lsid, occurrenceCountDTO);
 
                 return(
@@ -471,6 +541,20 @@
                     }
                 )
             });
+
+            const batCount = occurrenceCountDTO[occurrenceCountDTO.length-1];
+            if (batCount.additional.scientificName==="Chiroptera"){
+                result.push({
+                    scientificName:batCount.additional.scientificName,
+                    commonName:batCount.additional.commonName,
+                    taxonGuid:batCount.additional.taxonGuid,
+                    lastRecorded:batCount.year,
+                    count:batCount.count,
+                    sensitiveInEngland:true,
+                    sensitiveInWales:false
+                });
+            }
+            return result;
         }
 
         /**
